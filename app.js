387d90b8cd21c4259a0e5f6dcced639e0d2248af
Justin Gehr <gehrjay@gmail.com>
2017-08-09 22:21:45 -0500

    got bare-bones form written up, just need add functionality

--- app.js
+++ app.js
@@ -9,16 +9,23 @@ const path = require('path');
 
 let app = express();
 
+// this is the middleware for routes
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
+// I am serveing up static files here
 app.use(express.static('./public'));
+app.use('/bootstrap',express.static(path.resolve(__dirname, 'node_modules/bootstrap/dist')))
+app.use('/jquery',express.static(path.resolve(__dirname, 'node_modules/jquery/dist')))
 app.get('*', function(req, res) {
   res.sendFile(path.resolve(__dirname, 'public/index.html'));
 });
+
+// sending any api route over to routes
 app.use('/api', routes);
 
+// syncing database and starting server
 models.db.sync({force: true})
 .then(function() {
     app.listen(3000,function() {
--- browser/react/ReferralPage.jsx
+++ browser/react/ReferralPage.jsx
@@ -4,6 +4,40 @@ import React from 'react';
 
 export default class ReferralPage extends React.Component {
     render() {
-        return <h1>Hello</h1>
+        return (
+            <div className="Referral page container">
+                <h2>Like your service? Refer us to your friends!</h2>
+                <h2>Leave their information below and we will handle the rest</h2>
+                <form className="form-horizontal">
+                    <fieldset>
+                        <div className="form-group col-lg-12 col-md-12">
+                            <label>First Name</label>
+                            <input
+                                className="form-control"
+                                name="First Name"
+                                data-type="firstName"
+                            />
+                            <label>Last Name</label>
+                            <input
+                                className="form-control"
+                                name="Last Name"
+                                data-type="lastName"
+                            />
+                            <label>Email</label>
+                            <input
+                                className="form-control"
+                                name="Email"
+                                data-type="email"
+                            />
+                        </div>
+                        <div className="form-group col-lg-12 col-md-12">
+                            <button type="submit" className="btn btn-default">
+                                Submit Referral
+    	                    </button>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
+        )
     }
 }
