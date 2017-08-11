9ee5fdbcbb24844211f6f898da354d091912ae9f
Justin Gehr <gehrjay@gmail.com>
2017-08-11 12:51:07 -0500

    working?

--- models/index.js
+++ models/index.js
@@ -3,21 +3,11 @@
 // would normally make each model in the database its own module but since this project is so small I decided to just put it all in the index.js file
 
 const Sequelize = require('sequelize');
-  if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
-    // the application is executed on Heroku ... use the postgres database
-    sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
-      dialect:  'postgres',
-      protocol: 'postgres',
-      port:     match[4],
-      host:     match[3],
-      logging:  true //false
-    })
-    else {
 
 let db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/AutoMech', {
     logging: false
 });
-    }
+    
 
 // define user model here, I'm sure an actual user would have more data in their model but for purpose of project this seems fine
 
